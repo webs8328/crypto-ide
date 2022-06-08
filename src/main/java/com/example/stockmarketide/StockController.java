caaeffee652fa4e2097fa2bf449ad568e142b054
Jonathon Webster <webs8328@berkeley.edu>
2022-06-07 17:23:09 -0700

    updated names

--- src/main/java/com/example/stockmarketide/StockController.java
+++ src/main/java/com/example/stockmarketide/StockController.java
@@ -3,7 +3,7 @@ package com.example.stockmarketide;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class StockController {
     @FXML
     private Label welcomeText;
 
