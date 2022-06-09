2fd075bf741c9c52dda72ab9450a0cf05e830dee
Sebastian Ornelas <sebastianornelas@berkeley.edu>
2022-06-09 00:04:41 -0700

    rough draft API Class made

---
+++ src/main/java/com/example/stockmarketide/API.java
@@ -0,0 +1,63 @@
+package com.example.stockmarketide;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class API {
+
+    //Returns JSONObject of all requested coins and their respective variables
+
+    //reqData format:
+        //
+        //  coins = [bitcoin, ethereum, etc.]
+        //  variables = [price, supply, etc.]
+        //
+
+    public static JSONObject fetch(String[] coins, String[] variables) {
+        JSONObject results = new JSONObject();
+
+        try {
+            String coinString = String.join(",", coins);
+            URL url = new URL("api.coincap.io/v2/assets?ids=" + coinString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 200) {
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+                while (scanner.hasNext()) {
+                    informationString.append(scanner.nextLine());
+                }
+                scanner.close();
+
+                JSONParser parse = new JSONParser();
+                JSONObject res = (JSONObject) parse.parse(String.valueOf(informationString));
+                JSONArray resData = (JSONArray) res.get("data");
+
+                for (int i = 0; i < coins.length; i++) {
+                    JSONObject coin = new JSONObject();
+                    JSONObject coinResData = (JSONObject) resData.get(i);
+                    for (String variable : variables) {
+                        coin.put(variable, coinResData.get(variable));
+                    }
+                    results.put(coins[i], coin);
+                }
+        //also handle this later
+            } else {
+                throw new RuntimeException("Response Code: " + responseCode);
+            }
+        //handle this catch later
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.stockmarketide {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
 
     opens com.example.stockmarketide to javafx.fxml;
