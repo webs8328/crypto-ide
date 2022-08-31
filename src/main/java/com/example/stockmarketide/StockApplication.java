5e5af5f7d47b875d9bf965486a20ec4c03ef15b4
Jonathon Webster <webs8328@berkeley.edu>
2022-08-30 23:20:49 -0700

    ye

--- src/main/java/com/example/stockmarketide/StockApplication.java
+++ src/main/java/com/example/stockmarketide/StockApplication.java
@@ -3,6 +3,7 @@ package com.example.stockmarketide;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,8 @@ public class StockApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StockApplication.class.getResource("stockideview.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Stock Market IDE");
+        stage.setTitle("CRYPTO IDE");
+        stage.getIcons().add(new Image("file:icon.png"));
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show();
